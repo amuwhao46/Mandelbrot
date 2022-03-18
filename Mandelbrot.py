4856a73af4599d87e8b9308b294a815d15704004
Oke Amuwha <amuwhao46@gmail.com>
2022-03-18 14:49:30 -0400

    Function to check for stable numbers in mandelbrot set
    Checks number in the range of 2 for bounded numbers to be used in the mandelbrot set

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -1,10 +1,29 @@
 import matplotlib.pyplot as plt
 import numpy as np
+np.warnings.filterwarnings("ignore")
 
+#-=Functions=-
+
+#Prereq for graphing complex plain
+def complex_matrix(xmin, xmax, ymin, ymax, pixel_density):
+    re = np.linspace(xmin, xmax, int((xmax - xmin) * pixel_density))
+    im = np.linspace(ymin, ymax, int((ymax - ymin) * pixel_density))
+    return re[np.newaxis, :] + im[:, np.newaxis] * 1j
+
+#Recursion to check for valid set
+def isStable(c, numItr):
+    z = 0
+    for _ in range(numItr):
+        z = z ** 2 + c
+    return abs(z) <= 2
+
+
+
+#Testing Mandelbrot set
 """
 Recursive function to check
 if n is bounded
-"""
+
 def z(n, c):
     if n == 0:
         return 0
@@ -14,9 +33,4 @@ def z(n, c):
 #Testing function
 for n in range(10):
     print(f"z({n}) = {z(n, c=0)}")
-
-#Prereq for graphing complex plain
-def complex_matrix(xmin, xmax, ymin, ymax, pixel_density):
-    re = np.linspace(xmin, xmax, int((xmax - xmin) * pixel_density))
-    im = np.linspace(ymin, ymax, int((ymax - ymin) * pixel_density))
-    return re[np.newaxis, :] + im[:, np.newaxis] * 1j
+"""
