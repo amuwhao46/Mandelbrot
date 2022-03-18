385fe811cd38609e6362b9b2290361c2df4521c0
Oke Amuwha <amuwhao46@gmail.com>
2022-03-18 14:31:28 -0400

    Function to graph complex plain
    The function added will interpret each complex number (in a matrix) as a certain pixel on a screen

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -13,4 +13,10 @@ def z(n, c):
 
 #Testing function
 for n in range(10):
-    print(f"z({n}) = {z(n, c=-1)}")
+    print(f"z({n}) = {z(n, c=0)}")
+
+#Prereq for graphing complex plain
+def complex_matrix(xmin, xmax, ymin, ymax, pixel_density):
+    re = np.linspace(xmin, xmax, int((xmax - xmin) * pixel_density))
+    im = np.linspace(ymin, ymax, int((ymax - ymin) * pixel_density))
+    return re[np.newaxis, :] + im[:, np.newaxis] * 1j
